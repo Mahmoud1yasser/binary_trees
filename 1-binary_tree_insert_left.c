3dd1b58ab6a2d86d74ea8010646963d721796a09
Mahmoud1yasser <mahmoudyasser7388@yahoo.com>
2023-11-29 21:18:41 +0200

    update

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -7,11 +7,10 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	if (parent == NULL)
-		return (NULL);
-
 	binary_tree_t *left_node;
 
+	if (parent == NULL)
+		return (NULL);
 	left_node = malloc(sizeof(binary_tree_t));
 	if (left_node == NULL)
 		return (NULL);
