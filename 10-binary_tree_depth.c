d97148846462fd623304fc92512e1f9256132e8b
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-11-30 20:11:27 +0200

    abdo

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_depth - depth of node from root
+ * @tree: node check the depth
+ * Return: 0 is it is the root or number of depth
+*/
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
+}
